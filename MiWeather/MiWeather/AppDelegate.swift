f500d0103063442087b6e03b6d43539580ce34ee
Oussef Fersi <oussef.fersi@gmail.com>
2023-03-09 23:44:13 +0100

    First commit

---
+++ MiWeather/MiWeather/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MiWeather
+//
+//  Created by oussef on 09/03/2023.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window:UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ MiWeather/MiWeather/ListCityScene/ListCitiesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListCitiesViewController.swift
+//  MiWeather
+//
+//  Created by oussef on 09/03/2023.
+//
+
+import UIKit
+
+class ListCitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
